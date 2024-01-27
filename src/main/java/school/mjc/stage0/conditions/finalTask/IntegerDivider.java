dfd321ce001b124dcfe47df35ce423b77d777334
niluwka0403 <ninimamatova0403@gmail.com>
2024-01-27 20:42:39 +0500

    Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int result;
+        int multiply;
+        if (dividend % 2 == 0 && divider % 2 == 0 ) {
+result = dividend / divider;
+multiply = result * divider;
+if ( multiply == divider){
+    System.out.println("can be divided completely");
+}else {
+    System.out.println("cannot be divided completely");
+}
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,31 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        String season;
+        switch (monthNumber){
+            case (1):
+            case (2):
+            case (12):
+                season = "winter";
+                break;
+            case (3):
+            case (4):
+            case (5):
+                season = "spring";
+                break;
+            case (6):
+            case (7):
+            case (8):
+                season = "summer";
+                break;
+            case (9):
+            case (10):
+            case (11):
+                season = "autumn";
+                break;
+            default:
+                season = "Wrong month number!";
+                break;
+        }
     }
 }
